3782af427311cd15f98611b805b99c69bd4193f1
itslhun <nahuelghastin@outlook.com>
2021-10-12 19:20:00 +0200

    complete iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,62 @@ import contacts from './contacts.json';
 const fiveContacts = contacts.slice(0, 5);
 
 class App extends Component {
-  displayContacts = () => {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentContacts: contacts.slice(0, 5),
+      remainingContacts: contacts.slice(5)
+    };
+  }
+  displayContacts = () => {
+    const elements = this.state.currentContacts.map((contact) => {
+      return (
+        <tr key={`${contact.name}${Math.random()}`}>
+          <td>
+            <img
+              className="profile-img"
+              src={contact.pictureUrl}
+              alt="contact"
+            />
+          </td>
+          <td>
+            <p>{contact.name}</p>
+          </td>
+          <td>
+            <p>{contact.popularity}</p>
+          </td>
+        </tr>
+      );
+    });
+    return elements;
+  };
 
+  addRandomContact = () => {
+    //avoiding direct mutation of the state
+    const newContacts = [...this.state.currentContacts];
+    const remainingContacts = [...this.state.remainingContacts];
+
+    // get random number from the remaining, not added contacts
+    const randomNumber = Math.floor(
+      Math.random() * this.state.remainingContacts.length
+    );
+
+    newContacts.push(remainingContacts.splice(randomNumber, 1)[0]);
+
+    this.setState((previous) => {
+      return {
+        currentContacts: newContacts,
+        remainingContacts: remainingContacts
+      };
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <div className="buttons-wrapper">
+          <button onClick={this.addRandomContact}>Add Random Contact</button>
+        </div>
         <table>
           <thead>
             <tr>
@@ -21,27 +71,7 @@ class App extends Component {
               <th>Popularity</th>
             </tr>
           </thead>
-          <tbody>
-            {fiveContacts.map((contact) => {
-              return (
-                <tr>
-                  <td>
-                    <img
-                      className="profile-img"
-                      src={contact.pictureUrl}
-                      alt="contact"
-                    />
-                  </td>
-                  <td>
-                    <p>{contact.name}</p>
-                  </td>
-                  <td>
-                    <p>{contact.popularity}</p>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{this.displayContacts()}</tbody>
         </table>
       </div>
     );
